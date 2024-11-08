c8aad817d77d039294e21e3bc40bad9980398790
Daniel Ławniczak <daniel.lawniczak93@gmail.com>
2024-11-08 19:31:24 +0100

    *App initialization

---
+++ Assets/Code/Application/ApplicationManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ApplicationManager : MonoBehaviour
+{
+    public static event UnityAction<ScreenResolution> OnScreenResolutionChanged = null;
+
+    private static ApplicationManager instance = null;
+
+    [SerializeField] private InitializationBehaviour[] initializationBehaviours = new InitializationBehaviour[] { };
+    [SerializeField] private InitializationObject[] initializationObjects = new InitializationObject[] { };
+
+    private ScreenResolutionChecker screenResolutionChecker = new ScreenResolutionChecker();
+
+    private void Awake()
+    {
+        screenResolutionChecker.CheckForResolutionChange();
+    }
+
+    private IEnumerator Start()
+    {
+        yield return initializationBehaviours.HandleInitialization();
+        yield return initializationObjects.HandleInitialization();
+    }
+
+    private void Update()
+    {
+        if (screenResolutionChecker.CheckForResolutionChange())
+        {
+            OnScreenResolutionChanged?.Invoke(screenResolutionChecker.Resolution);
+        }
+    }
+}
---
+++ Assets/Code/Application/InitializationBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class InitializationBehaviour : MonoBehaviour, IInitializationModule
+{
+    public virtual IEnumerator HandleModuleInitialization()
+    {
+        yield break;
+    }
+}
